46ecdc18f232ecae4c202b17dd67d78e33d80754
Lavkush <lavkushdas7739@gmail.com>
2025-08-08 08:47:26 +0530

    Feat: dotenv config added.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import type { Request, Response } from "express";
+import dotenv from 'dotenv';
 
-const PORT = 3000;
+dotenv.config();
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello  and Good Morning!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Express app started at port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Express app started at port ${process.env.PORT}`);
 });
